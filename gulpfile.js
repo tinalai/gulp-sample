bbe43ccc2382e76760bf8eb32295d59f5f60bbbb
Tina Lai <tlai@helloelephant.com>
2018-05-02 10:31:51 -0700

    add gulp image task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 var
   // modules
   gulp = require('gulp'),
+  newer = require('gulp-newer'),
+  imagemin = require('gulp-imagemin'),
 
   // development mode?
   devBuild = (process.env.NODE_ENV !== 'production'),
@@ -12,3 +14,13 @@ var
     build: 'build/'
   }
 ;
+
+// IMAGE PROCESSING
+
+gulp.task('images', function(){
+  var out = folder.build + 'images/';
+  return gulp.src(folder.src + 'images/**/*')
+    .pipe(newer(out))
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest(out));
+});
