5f3c913edae3bf7f1c50ea476172ea3530700dba
Tina Lai <tlai@helloelephant.com>
2018-05-02 14:13:31 -0700

    add gulp css task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,16 @@ var
   newer = require('gulp-newer'),
   imagemin = require('gulp-imagemin'),
   htmlclean = require('gulp-htmlclean'),
+  concat = require('gulp-concat'),
+  deporder = require('gulp-deporder'),
+  stripdebug = require('gulp-strip-debug'),
+  uglify = require('gulp-uglify'),
+  sass = require('gulp-sass'),
+  postcss = require('gulp-postcss'),
+  assets = require('postcss-assets'),
+  autoprefixer = require('autoprefixer'),
+  mqpacker = require('css-mqpacker'),
+  cssnano = require('cssnano'),
 
   // development mode?
   devBuild = (process.env.NODE_ENV !== 'production'),
@@ -40,4 +50,43 @@ gulp.task('html', ['images'], function() {
   }
 
     return page.pipe(gulp.dest(out));
+});
+
+// JavaScript processing
+gulp.task('js', function() {
+  var jsbuild = gulp.src(folder.src + 'js/**/*')
+    .pipe(deporder())
+    .pipe(concat('main.js'));
+
+  if(!devBuild) {
+    jsbuild = jsbuild
+      .pipe(stripdebug())
+      .pipe(uglify());
+  }
+
+  return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
+});
+
+// CSS processing
+gulp.task('css', ['images'], function() {
+  var postCssOpts = [
+    assets({ loadPaths: ['images/'] }),
+    autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
+    mqpacker
+  ];
+
+  if (!devBuild) {
+    postCssOpts.push(cssnano);
+  }
+
+  return gulp.src(folder.src + 'scss/main.scss')
+    .pipe(sass({
+      outputStyle: 'nested',
+      imagePath: 'images/',
+      precision: 3,
+      errLogToConsole: true
+    }))
+    .pipe(postcss(postCssOpts))
+    .pipe(gulp.dest(folder.build + 'css/'));
+
 });
